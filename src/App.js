a73f01ecf1c1b1a4a57df68ef38e5116d9987e79
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2021-03-25 11:04:29 +0000

    seccion2  introducción a los props

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ function Box(props){
       <div>
          <h1>This is a stateless functional component</h1>
          <h1>{props.text}</h1>
+         <h2>{props.subtitle}</h2>
       </div>
     );
 }
 
 function App() {
   return (
-     <Box text="hi, Jose"/>
+      <Box 
+         text="hi, Jose"
+         subtitle="I hate this shit"
+      />
   );
 }
 
