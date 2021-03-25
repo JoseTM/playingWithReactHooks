3fda5811e3cf3a92842ff54581438eac277c4226
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2021-03-25 23:22:03 +0000

    seccion 2 primer contacto con useState

--- src/App.js
+++ src/App.js
@@ -1,45 +1,15 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react'
 
-class Button extends Component {
-   constructor(props){
-      super(props);
-      this.state = {
-         activated: false,
-      }
-      this.handleAtiveChange = this.handleAtiveChange.bind(this);  // binding de la función handleAcgtiveChange
-   }
+function App(){
+   console.log(useState(false));
 
-   handleAtiveChange(){
-      // this.setState({activated: !this.state.activated})
-      this.setState((prevState) => {
-         return{
-            activated: !prevState.activated
-         }
-      }
-      )
-   }
-
-   render(){
-      const buttonText = this.state.activated ? this.props.activeText: this.props.inactiveText;
-      return(
-         <button onClick={this.handleAtiveChange}>
-            {buttonText}
-         </button>
-      )
-   }
+   return(  
+      <button>
+         My Text
+      </button>
+   );
 }
 
 
-class  App extends Component {
-
-   render(){
-      return(
-         <Button 
-         activeText="on"
-         inactiveText="off"
-         />
-      )
-   }
-}
 
 export default App;
