3fbaa0989917477428b097e427b9532c23c6d07e
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2021-03-26 17:44:14 +0000

    sección 3 primer uso de useEffect

--- src/App.js
+++ src/App.js
@@ -1,57 +1,32 @@
-import React, {Component} from 'react'
+import React, {useState, useEffect} from 'react'
 
-class Counter extends Component{
-   constructor(props){
-      super(props);
-      this.state = {
-         count:  0,
-      };
-   };
+function App(){
+   
+   const [counter, setCounter] = useState(0);
+   const sumOnClick = () => setCounter(counter + 1);
+   const substractOnClick = () => setCounter(counter - 1);
+   const resetOnClick = () => setCounter(0);
 
-   componentDidMount(){
-      console.log("Im mounted, Im run just one time after first rendering");
-   }
-
-   componentDidUpdate(prevProps, prevState){
-      console.log("Im updated, Im run after any update of props "+prevState.count);
-   }
-
-   componentWillUnmount(){
-      console.log("Im been umounted, Im run just before component disapear from DOM")
-   }
-
-   render(){
-      console.log("im rendering");
-      const {count} = this.state;
-      return(
-         <div>
-            <button onClick={() => this.setState({count: count + 1})}>increase</button>
-            <button onClick={() => this.setState({count: count - 1})}>decrease</button>
-            <h1>{count}</h1>
-         </div>
+   useEffect(
+      () => console.log(`im inside useeffect current count is ${counter}`)
       );
-   }
-}
 
-class App extends Component {
-   constructor(props){
-      super(props);
-      this.state={
-         visible: false,
-      }
-   };
-
-   render(){
-      const { visible } = this.state;
-      return(
-         <div>
-            <button onClick={() =>this.setState({visible: !visible})}>
-               show/hide the counter component
-            </button>
-            { visible && <Counter/>}
-         </div>
+   return( 
+      <div>
+         <button onClick={sumOnClick}> 
+            increase
+         </button>
+         <button onClick={substractOnClick}> 
+            decrease
+         </button>  
+         <button onClick={resetOnClick}> 
+            reset
+         </button>  
+         <h1>
+            { counter }
+         </h1>
+      </div>
       );
-   }
 }
 
 export default App;
