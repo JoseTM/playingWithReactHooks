18e46d8c19975d2cb526c6c53b20a4d7c231e26b
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2021-03-26 01:06:32 +0000

    seccion 2 usames useState tantas veces como sea necesario dentro del componente para encapsular los datos que deben almacenarce en lugar de usar un objeto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React, {useState} from 'react'
 
 function App(){
-   const [place, setPlace] = useState({
-                                       city: '',
-                                       country: '',
-                                    });
+   const [city, setCity] = useState('')
+   const [country, setCountry] = useState('');
 
-   const handleCityChange = (event) => 
-      setPlace({  
-            ...place,
-            city: event.target.value,            
-            });
-   
-   const handleCountryChange = (event) => 
-      setPlace({
-            ...place,
-            country: event.target.value,
-            });
+   const handleCityChange = (event) =>  setCity(event.target.value);   
+   const handleCountryChange = (event) => setCountry(event.target.value);
    
    return( 
       <form>
@@ -24,20 +13,20 @@ function App(){
             <input 
                type="text" 
                placeholder="City" 
-               value={place.city}
-               onChange={handleCityChange}
+               value={ city }
+               onChange={ handleCityChange }
             />
          </div>
          <div>
             <input
                type="text" 
                placeholder="Country" 
-               value={place.country}
-               onChange={handleCountryChange}
+               value={ country }
+               onChange={ handleCountryChange }
             />
          </div>
          <div>
-            You live in {place.city}, {place.country}
+            You live in { `${city},  ${country}` }
          </div>
       </form>
    );
