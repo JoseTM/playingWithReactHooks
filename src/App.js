85657f53f68130901e6fd8b25e7e1108d8458850
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2021-03-26 00:19:45 +0000

    seccion 2 añadimos otros clicks handlers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, {useState} from 'react'
 
 function App(){
-
-   const [counter, setCounter] = useState(0)
-   const handleClick = () => setCounter(counter + 1);
+   console.log(useState(0));
+   const [counter, setCounter] = useState(0);
+   const sumOnClick = () => setCounter(counter + 1);
+   const substractOnClick = () => setCounter(counter - 1);
+   const resetOnClick = () => setCounter(0);
 
    return( 
       <div>
-         <button onClick={handleClick}> 
-            +1
-         </button>         
+         <button onClick={sumOnClick}> 
+            increase
+         </button>
+         <button onClick={substractOnClick}> 
+            decrease
+         </button>  
+         <button onClick={resetOnClick}> 
+            reset
+         </button>  
          <h1>
             { counter }
          </h1>
