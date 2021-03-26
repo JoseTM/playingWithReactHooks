751750532884f16aa44239e7cd7ddcee2b934916
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2021-03-26 00:51:55 +0000

    seccion 2 use object argument with hook

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import React, {useState} from 'react'
 
 function App(){
-   console.log(useState(0));
-   const [counter, setCounter] = useState(0);
-   const sumOnClick = () => setCounter(counter + 1);
-   const substractOnClick = () => setCounter(counter - 1);
-   const resetOnClick = () => setCounter(0);
+   const [place, setPlace] = useState({
+                                       city: '',
+                                       country: '',
+                                    });
 
+   const handleCityChange = (event) => 
+      setPlace({  
+            ...place,
+            city: event.target.value,            
+            });
+   
+   const handleCountryChange = (event) => 
+      setPlace({
+            ...place,
+            country: event.target.value,
+            });
+   
    return( 
-      <div>
-         <button onClick={sumOnClick}> 
-            increase
-         </button>
-         <button onClick={substractOnClick}> 
-            decrease
-         </button>  
-         <button onClick={resetOnClick}> 
-            reset
-         </button>  
-         <h1>
-            { counter }
-         </h1>
-      </div> 
+      <form>
+         <div>
+            <input 
+               type="text" 
+               placeholder="City" 
+               value={place.city}
+               onChange={handleCityChange}
+            />
+         </div>
+         <div>
+            <input
+               type="text" 
+               placeholder="Country" 
+               value={place.country}
+               onChange={handleCountryChange}
+            />
+         </div>
+         <div>
+            You live in {place.city}, {place.country}
+         </div>
+      </form>
    );
 }
 
