79dde79d5f644e52d8d4d8c5bdebbc536e6e3829
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2021-03-27 17:43:19 +0000

    seccion 5 hacemos un custom hook para el contador lo llamaos useCounter y lo aplicamos en dos componentes distintos Display y FancyDisplay

--- src/App.js
+++ src/App.js
@@ -1,36 +1,66 @@
-import React, {useState, useEffect} from 'react'
 
+import React, {useState} from 'react'
 
+function useCounter(initialValue = 0, stepValue = 1) {
+   const [counter, setCounter] = useState(initialValue);
+   const sumOnClick = () => setCounter(counter + stepValue);
+   const substractOnClick = () => setCounter(counter - stepValue);
+   const resetOnClick = () => setCounter(initialValue);
 
-function useKeyPress(startingValue){
-   const [userText, setUserText] = useState(startingValue);
-   
-   const handleUserKeyPress = (event) => {
-      const { key, keyCode } = event;
-      if ( keyCode === 32 || (keyCode >= 65 && keyCode <=90) ){
-         setUserText(`${userText}${key}`);
-      }
-      console.log(keyCode);
-   };
-
-   useEffect(
-      () => {
-         window.addEventListener('keydown', handleUserKeyPress);
-         return () => window.removeEventListener('keydown', handleUserKeyPress);
-      });
-   return userText;
+   return {counter, sumOnClick, substractOnClick, resetOnClick};
 }
+
+function Display(props) {
+   const {counter, sumOnClick, substractOnClick, resetOnClick} = useCounter(props.start, props.step);
+   
+   return (
+      <div id="display">
+         <button onClick={sumOnClick}> 
+            increase
+         </button>
+         <button onClick={substractOnClick}> 
+            decrease
+         </button>  
+         <button onClick={resetOnClick}> 
+            reset
+         </button>  
+         <h1>
+            { counter }
+         </h1>
+      </div> 
+   );
+}
+
+function FuncyDisplay(props) {
+   const {counter, sumOnClick, substractOnClick, resetOnClick} = useCounter(props.start, props.step);
+   
+   return (
+      <section id="funcyDisplay">
+         <button onClick={sumOnClick}> 
+            increase
+         </button>
+         <button onClick={substractOnClick}> 
+            decrease
+         </button>  
+         <button onClick={resetOnClick}> 
+            reset
+         </button>  
+         <h1>
+            { counter }
+         </h1>
+      </section> 
+   );
+}
+
 
 function App() {
    
-   const userText = useKeyPress('En un lugar de la mancha ');
-
-   return (
+   return( 
       <div>
-         <h1>Feel free to type!</h1>
-         <blockquote>
-            { userText }
-         </blockquote>
+         <Display start={10}/>
+         <Display start={20} step={4}/>
+         <Display />
+         <FuncyDisplay start={50}/>
       </div>
    );
 }
