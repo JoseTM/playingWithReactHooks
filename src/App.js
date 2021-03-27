253468d7da2d9ada7922ec4ec12af1f1965804b4
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2021-03-27 01:17:45 +0000

    seccion 3 usando userEffect con eventos para capturar la entrada de teclado lección 19 CONCEPTOS IMPORTANTES

--- src/App.js
+++ src/App.js
@@ -1,53 +1,30 @@
 import React, {useState, useEffect} from 'react'
 
 
-function Counter(){
-   const [counter, setCounter] = useState(0);
-   const [color, setColor] = useState('salmon');
-   const sumOnClick = () => setCounter(counter + 1);
-   const substractOnClick = () => setCounter(counter - 1);
-   const resetOnClick = () => setCounter(0);
-   const togglecolor = () => (color === 'salmon') ? setColor('green') : setColor('salmon') ;
+function App() {
+   const [userText, setUserText] = useState('')
+   
+   const handleUserKeyPress = (event) => {
+      const { key, keyCode } = event;
+      if ( keyCode === 32 || (keyCode >= 65 && keyCode <=90) ){
+         setUserText(`${userText}${key}`);
+      }
+      console.log(keyCode);
+   };
 
    useEffect(
       () => {
-            console.log(`Im inside useeffect current. Count is ${counter}`);
+         window.addEventListener('keydown', handleUserKeyPress);
+         return () => window.removeEventListener('keydown', handleUserKeyPress);
+      });
 
-            return  () => console.log(`Im removing anything that was setup above. The last count was ${counter}`);
-         },
-      [color]
-      );
-
-   return( 
-      <div>
-         <button onClick={sumOnClick}> 
-            increase
-         </button>
-         <button onClick={substractOnClick}> 
-            decrease
-         </button>  
-         <button onClick={resetOnClick}> 
-            reset
-         </button>  
-         <button onClick={togglecolor}> 
-            change color
-         </button>  
-         <h1 style={{color}} >
-            { counter }
-         </h1>
-      </div>
-      );
-}
-
-
-function App(){
-   const [visible, setVisible] = useState(false);
-   const toggleComponentVisibility = () => setVisible(!visible);
 
    return (
       <div>
-         <button onClick={toggleComponentVisibility} >hide/show counter component</button>
-         {visible && <Counter />}         
+         <h1>Feel free to type!</h1>
+         <blockquote>
+            { userText }
+         </blockquote>
       </div>
    );
 }
